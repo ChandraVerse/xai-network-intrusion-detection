385bc4982e6f922fc0761d294ec027db00db9660
Chandra Sekhar Chakraborty <chakrabortychandrasekhar185@gmail.com>
2026-04-05 15:16:23 +0530

    fix: add proper exports to src/utils/__init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,15 @@
 """Utility modules for XAI-NIDS."""
+
+from .logger import get_logger
+from .metrics import compute_metrics, print_metrics_table, format_metrics_for_dashboard
+from .report_generator import ReportGenerator
+from .pcap_converter import PcapConverter
+
+__all__ = [
+    "get_logger",
+    "compute_metrics",
+    "print_metrics_table",
+    "format_metrics_for_dashboard",
+    "ReportGenerator",
+    "PcapConverter",
+]
